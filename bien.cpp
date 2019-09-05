534ed63a5c0ac4a30585e07e7faeaf51b0f37986
phuong2001 <tranvanphuong01012001@gmail.com>
2019-09-05 13:33:12 +0700

    bai

---
+++ bien.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int x,y,a;
+	scanf("%d",&x);
+	scanf("%d",&y);
+	x=y;
+	
+	y=a;
+	a=x;
+	printf("%d\n",x);
+	printf("%d\n",y);
+	return 0;
+	}
---
+++ so sanh.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	if (a>b){
+	printf ("%d la solon hon",a);
+	}
+	else {
+		printf ("%d la so lon hon",b);
+		
+		}
+		
+		return 0;
+		}
