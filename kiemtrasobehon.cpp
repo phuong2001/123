5f9f5f796c3a04209c4d89ef084d4f4591a0139b
phuong2001 <tranvanphuong01012001@gmail.com>
2019-09-06 13:36:30 +0700

    bai

---
+++ kiemtrasobehon.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int n;
+	int i;
+	scanf ("%d",&n);
+	
+			while(i<n){
+		if(i%2==0){
+	printf ("so chan be hon %d\n",i);
+			}	
+			
+				i++;
+				
+			}
+		
+		
+			
+		return 0;
+		}
+	
