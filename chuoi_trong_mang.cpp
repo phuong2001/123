92831b17ccb395c7ce18515c7ff5fe6e3376646c
phuong2001 <tranvanphuong01012001@gmail.com>
2019-09-17 13:19:35 +0700

    chuoi

---
+++ chuoi_trong_mang.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char ary[3];
+	int n;
+
+	char s[n]	;
+	printf("nhap chuoi\n");
+	scanf("%s",s);
+
+	
+	for(int i = 0;i<s[n];i++){
+		if(ary[i]==s[n]){
+			printf("Co trong mang\n",ary[i]);
+			return 0;
+		}
+	}
+		printf("Khong co trong mang\n");
+		return 0;
+	}
+
+	
